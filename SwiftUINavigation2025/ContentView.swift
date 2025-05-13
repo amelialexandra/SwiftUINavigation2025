edd9de97f84b3901accdb270223196edf8e13eb6
amelialexandra <amelialexandra.it@gmail.com>
2025-05-13 07:00:04 +0700

    Initial Commit

---
+++ SwiftUINavigation2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUINavigation2025
+//
+//  Created by Amelia Alexandra on 13/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUINavigation2025/SwiftUINavigation2025App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigation2025App.swift
+//  SwiftUINavigation2025
+//
+//  Created by Amelia Alexandra on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigation2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
